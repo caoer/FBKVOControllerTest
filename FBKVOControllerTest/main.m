43b18b2425444d07b0a173bd8e4cfb11313659d6
Zitao Xiong <caoer115@gmail.com>
2015-08-28 17:26:35 -0400

    add more example

--- FBKVOControllerTest/main.m
+++ FBKVOControllerTest/main.m
@@ -12,7 +12,7 @@
 @interface Person : NSObject
 
 @property (strong) NSString *name;
-
+@property (nonatomic, strong) Person *child;
 @end
 
 @implementation Person
@@ -20,8 +20,8 @@
 //    [self.KVOControllerNonRetaining unobserveAll];
 //}
 - (void)dealloc {
-    [self.KVOControllerNonRetaining unobserveAll];
-    NSLog(@"I got deallocaed");
+    [self fb_unobserveAll];
+    NSLog(@"I got deallocaed, my name is %@", self.name);
 }
 @end
 
@@ -29,23 +29,61 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         Person *p1 = [Person new];
-//        p1.KVOControllerNonRetaining;
-        
+       p1.name = @"p1";
         // EXAMPLE 1
         {
             Person *p2 = [Person new];
+            p2.name = @"p2";
             [p2.KVOControllerNonRetaining observe:p2 keyPath:@"name" options:0 block:^(id observer, id object, NSDictionary *change) {
                 NSLog(@"p2 changed!");
             }];
         }
         
-        // EXAMPLE 2
+        // EXAMPLE 2 [CRASH] observer has no link to observee, if observee got dealloc first, it will cause crash.
+        // See example 2A compared to 2B
 //        {
 //            Person *p3 = [Person new];
 //            [p1.KVOControllerNonRetaining observe:p3 keyPath:@"name" options:0 block:^(id observer, id object, NSDictionary *change) {
 //                NSLog(@"p3 changed!");
 //            }];
 //        }
+        // Example 2A [CRASH]
+//        {
+//            Person *p4 = [Person new];
+//            Person *p5 = [Person new];
+//            [p4.KVOControllerNonRetaining observe:p5 keyPath:@"name" options:0 block:^(id observer, id object, NSDictionary *change) {
+//                NSLog(@"p5 changed");
+//            }];
+//        }
+
+        //Example 2B
+        {
+            Person *p4 = [Person new];
+            p4.name = @"P4";
+            Person *p5 = [Person new];
+            p5.name = @"P5";
+            [p5.KVOControllerNonRetaining observe:p4 keyPath:@"name" options:0 block:^(id observer, id object, NSDictionary *change) {
+                NSLog(@"p5 changed");
+            }];
+        }
+        // EXAMPLE 4
+        {
+            Person *parent = [Person new];
+            Person *child = [Person new];
+            parent.child = child;
+            
+            [parent.KVOControllerNonRetaining observe:parent keyPath:@"name" options:0 block:^(id observer, id object, NSDictionary *change) {
+                NSLog(@"parent got change");
+            }];
+            [parent.KVOControllerNonRetaining observe:child keyPath:@"name" options:0 block:^(id observer, id object, NSDictionary *change) {
+                NSLog(@"parent observe child and child got change");
+            }];
+            [parent.KVOControllerNonRetaining observe:parent keyPath:@"child.name" options:0 block:^(id observer, id object, NSDictionary *change) {
+                NSLog(@"parent observe parent and child got change");
+            }];
+            parent.name = @"parent";
+            child.name = @"child";
+        }
     }
     return 0;
 }
---
+++ README.md
@@ -0,0 +1,19 @@
+        [self.KVOController observe:self keyPath:@"name" options:NSKeyValueObservingOptionInitial|NSKeyValueObservingOptionNew block:^(id observer, id object, NSDictionary *change) {
+            NSLog(@"got change %@", change);
+            NSLog(@"self'name is %@", self.name);
+        }];
+
+Above code has two issues. 
+1. If using self.KVOController or self.KVOControllerNonRetaining observe 'self'. It needs to use KVOControllerNonRetaining instead of KVOController to avoid retain cycle. 
+2. When using self.KVOController, 'self' inside the block will create a retain cycle as well. need to weakify self. 
+
+        [self.KVOControllerNonRetaining observe:self keyPath:@"name" options:NSKeyValueObservingOptionInitial|NSKeyValueObservingOptionNew block:^(id observer, id object, NSDictionary *change) {
+            NSLog(@"got change %@", change);
+        }];
+
+After change it to KVOControllerNonRetaining, 'self' is responsible for unobserving it. otherwise, it will crash. However, there is a related PR to improve it: 
+https://github.com/facebook/KVOController/pull/67
+
+When using KVOControllerNonRetaining, it is unsafe to call unobserveAll in dealloc method. Due to it is lazyly initilized, it will crash by the time of calling dealloc, KVOControllerNonRetaining hasn't initilized, trying to create it in dealloc will cause the crash. A workaround is placed in https://github.com/caoer/KVOController/commit/6c4faa846a3b0634867850aec7b488538cd81913. Related issue discussion: https://github.com/facebook/KVOController/issues/46
+
+
